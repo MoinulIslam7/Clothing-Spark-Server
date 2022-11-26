b872e2c60341bafa08e58e905253796582262008
MoinulIslam7 <moinul180180@gmail.com>
2022-11-26 17:24:14 +0600

    load data from mongodb

--- index.js
+++ index.js
@@ -1,27 +1,49 @@
 const express = require('express');
 const cors = require('cors');
-const port =  process.env.PORT || 5000;
-
+const port = process.env.PORT || 5000;
+require('dotenv').config();
 const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
 
 // middlewares
 app.use(cors());
 app.use(express.json());
 
-// load products
-const products = require('./Products.json');
-app.get('/products', (req, res) => {
-    res.send(products)
-})
-app.get('/products/:id', (req, res) => {
-    const id = req.params.id;
-    const seletedProduct= products.find(n => n.id === id)
-    res.send(seletedProduct);
-})
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.0tydy0p.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const productsCollection = client.db("clothingSpark").collection("productsCategory");
+
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const cursor = productsCollection.find(query);
+            const categories = await cursor.toArray();
+            res.send(categories);
+        })
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const categoryProduct = await productsCollection.findOne(query);
+            res.send(categoryProduct);
+        })
+
+
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.log);
+
 
 app.get('/', async (req, res) => {
     res.send('Clothing sparks server running on server')
 })
-app.listen(port , () => {
+app.listen(port, () => {
     console.log("Clothing Spark running on port ", port);
 })
